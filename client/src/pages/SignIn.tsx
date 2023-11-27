c0ad567e9a795d16a0c7c97ec29034c989d87fc5
meghna0725 <meghna@pipeiq.ai>
2023-11-27 15:57:37 -0500

    sign in page styled

---
+++ client/src/pages/SignIn.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../styles/SignIn.css';
+
+const SignIn = () => {
+    return (
+        <div>
+            <h1 className="sign-in-title">Sign In</h1>
+            <div className="box-div">
+            </div>
+        </div>
+    );
+};
+
+export default SignIn;
