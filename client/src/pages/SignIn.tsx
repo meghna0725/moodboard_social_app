91cb183fe69357af1403c135dffd6855ededeca0
meghna0725 <meghna@pipeiq.ai>
2023-11-27 23:28:57 -0500

    added input fields to sign in page and returns fields as a json object

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -2,10 +2,26 @@ import React from "react";
 import '../styles/SignIn.css';
 
 const SignIn = () => {
+
+    // put username and password in a json object and console.log it
+    const handleClick = () => {
+        const username = (document.querySelector('.input-box-username') as HTMLInputElement).value;
+        const password = (document.querySelector('.input-box-password') as HTMLInputElement).value;
+        const user = {
+            username: username,
+            password: password
+        }
+        console.log(user);
+    }
+
+
     return (
         <div>
             <h1 className="sign-in-title">Sign In</h1>
             <div className="box-div">
+                <input className="input-box-username" type="text" placeholder="Username" />
+                <input className="input-box-password" type="password" placeholder="Password" />
+                <button className="sign-in-button" onClick={handleClick}>Sign In</button>
             </div>
         </div>
     );
